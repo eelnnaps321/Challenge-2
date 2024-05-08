4c7ba99407fe5b34f2d9f98b8b1f3347e9e064ff
leespann <spannl@roverkids.org>
2024-05-08 13:52:31 -0400

    challenge 2 complete

--- Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,6 +16,7 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
@@ -25,8 +26,18 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        //random ball from array
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+
+        //random spawnInterval
+        spawnInterval = Random.Range(0.5f,3.0f);
+
+        //Invoke
+        Invoke("SpawnRandomBall", spawnInterval);
+        Debug.Log("spawnInterval: " + spawnInterval);
+
     }
 
 }
